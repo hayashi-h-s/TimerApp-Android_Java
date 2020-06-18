e6c1fd5e3cf2549c24d5cb2ffae0774a60623db4
Haya <max9173627@gmail.com>
2020-06-18 10:21:09 +0900

    [add]クリック時の挙動を追加

--- app/src/main/java/com/haya/timerapp/MainActivity.java
+++ app/src/main/java/com/haya/timerapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.haya.timerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView TimerView = findViewById(R.id.timer_view);
+        Button StartPauseButton = findViewById(R.id.start_pause_button);
+        Button RsetButton = findViewById(R.id.reset_button);
+
+        StartPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Log.i("クリックテスト", "出力確認");
+
+
+
+            }
+        });
     }
 }
